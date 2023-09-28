e84d561164f449f116c3d97fabb98c3ac305b1ac
masud <masudranaacb@gmail.com>
2023-09-29 00:33:37 +0600

    added array push methods

---
+++ array_method.js
@@ -0,0 +1,4 @@
+const foods=["potato","banana","chili"]
+foods.push('tomato')
+foods.shift()
+console.log(foods)
---
+++ parseint.js
@@ -0,0 +1,7 @@
+let number = 10;
+let total = 0
+for (i = 1; i <= number; i++) {
+    // total = total + i
+    console.log(i)
+}
+// console.log(total)
