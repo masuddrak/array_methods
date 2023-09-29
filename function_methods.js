d15dd44b187efacaa5dd508ad489223bfdf2d6ce
masud <masudranaacb@gmail.com>
2023-09-29 08:13:55 +0600

    funtion just one return valu

---
+++ function_methods.js
@@ -0,0 +1,9 @@
+
+// funtional Methods Discusion
+// --------------------------
+// funtional additional operation
+function familly(a,b,c){
+return b,c,a
+}
+const result=familly("ali","salam")
+console.log(result)
