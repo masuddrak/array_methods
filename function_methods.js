134d7769e62ac619b9d9e5500c7082f7b12818f3
masud <masudranaacb@gmail.com>
2023-09-29 08:23:55 +0600

    funtion average crate

--- function_methods.js
+++ function_methods.js
@@ -1,9 +1,7 @@
-
-// funtional Methods Discusion
-// --------------------------
-// funtional additional operation
-function familly(a,b,c){
-return b,c,a
+function getAsignment(asignment1, asignment2, asignment3) {
+    const total = asignment1 + asignment2 + asignment3
+    const average = total / 3
+    return average
 }
-const result=familly("ali","salam")
+const result=getAsignment(50,55,60)
 console.log(result)
